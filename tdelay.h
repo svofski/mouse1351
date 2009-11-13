332c0346550a46836b998c7132c77219b574e303
svofski <svofski@432987f8-cff4-11de-9889-db2f8e67c15c>
2009-11-13 01:40:52 +0000

    test

--- tdelay.h
+++ tdelay.h
@@ -1,11 +1,12 @@
 //!\file
-//!\brief An attempt to make a usable delay routine
-//!
+//!\brief An attempt to make a usable delay routine without inlining.
+//! 
 #ifndef _TDELAY_H
 #define _TDELAY_H
 
 void tdelay(uint16_t);
 
 #endif
+
 
 //$Id$
